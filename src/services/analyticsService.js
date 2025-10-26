dad2e9a080a74d89020dead2ae06edbbde65c4d3
Zennara <dev@zennara.in>
2025-10-26 16:08:51 +0530

    Update analyticsService.js

--- src/services/analyticsService.js
+++ src/services/analyticsService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { API_BASE_URL } from '../config/api';
 
-const API_URL = 'http://localhost:5000/api/admin/analytics';
+const API_URL = `${API_BASE_URL}/api/admin/analytics`;
 
 // Get auth token
 const getAuthToken = () => {
